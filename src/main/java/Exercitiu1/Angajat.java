09708f7b157c458ec591532950e75aa53e02da28
danielp27 <danielpascu645@outlook.com>
2023-11-03 13:30:02 +0200

    m

--- src/main/java/Exercitiu1/Angajat.java
+++ src/main/java/Exercitiu1/Angajat.java
@@ -1,2 +1,121 @@
-package Exercitiu1;public class Angajat {
+package Exercitiu1;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.function.Predicate;
+
+@FunctionalInterface
+interface Filtru<T>{
+    public boolean test(T a);
+}
+
+public class Angajat {
+    private String nume;
+    private String post;
+    private LocalDate data_angajarii;
+    private float salar;
+
+    public Angajat(String nume, String post, LocalDate data_angajarii, float salar) {
+        this.nume = nume;
+        this.post = post;
+        this.data_angajarii = data_angajarii;
+        this.salar = salar;
+    }
+
+    public Angajat() {
+    }
+
+    @Override
+    public String toString() {
+        return "Angajat{" +
+                "nume='" + nume + '\'' +
+                ", post='" + post + '\'' +
+                ", data_angajarii=" + data_angajarii +
+                ", salar=" + salar +
+                '}';
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public float getSalar() {
+        return salar;
+    }
+
+    public LocalDate getData_angajarii() {
+        return data_angajarii;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setData_angajarii(LocalDate data_angajarii) {
+        this.data_angajarii = data_angajarii;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public void setSalar(float salar) {
+        this.salar = salar;
+    }
+
+    public static void scriere(List<Angajat> lista) {
+        try {
+            ObjectMapper mapper=new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            File file=new File("src/main/resources/angajati.json");
+            mapper.writeValue(file,lista);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Angajat> citire() {
+        try {
+            File file=new File("src/main/resources/angajati.json");
+            ObjectMapper mapper=new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            List<Angajat> angajati = mapper.readValue(file, new TypeReference<List<Angajat>>(){});
+            return angajati;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void afisare(List <Angajat> angajati){
+        for (Angajat a:angajati) {
+            System.out.println(a);
+        }
+    }
+    public static void afisare_filtrata1(List <Angajat> angajati, Filtru<Angajat> f) {
+        for (Angajat a : angajati) {
+            if (f.test(a))
+                System.out.println(a);
+        }
+    }
+    public static void afisare_filtrata2(List <Angajat> angajati, Filtru<Angajat> f) {
+        for (Angajat a : angajati) {
+            if (f.test(a))
+                System.out.println(a.getSalar());
+
+        }
+    }
+
 }
--- src/main/java/Exercitiu1/Exercitiu1.java
+++ src/main/java/Exercitiu1/Exercitiu1.java
@@ -1,2 +1,68 @@
-package Exercitiu1;public class Exercitiu1 {
+package Exercitiu1;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Exercitiu1 {
+    public static void main(String[] args) {
+        List<Angajat> angajati = new ArrayList<>();
+        angajati = Angajat.citire();
+        int opt;
+        Scanner scanner = new Scanner(System.in);
+
+        do{
+            System.out.println("Meniu");
+            System.out.println("0-Iesire");
+            System.out.println("1. Afișarea listei de angajați\n" +
+                    "2. Afișarea angajaților care au salariul peste 2500 RON\n" +
+                    "3. Crearea unei liste cu angajații din luna aprilie, a anului trecut, care au funcție de\n" +
+                    "conducere (postul conține unul din cuvintele sef sau director).\n" +
+                    "4. Afișarea angajaților care nu au funcție de conducere (postul lor nu conține cuvintele\n" +
+                    "director sau șef), în ordine descrescătoare a salariilor \n"+
+                    "5. Extragerea din lista de angajați a unei liste de String-uri care conține numele angajaților\n" +
+                    "scrise cu majuscule.\n" +
+                    "6. Afișarea salariilor mai mici de 3000 de RON (doar salariile, fără alte informații)\n" +
+                    "7. Afișarea datelor primului angajat al firmei.\n" +
+                    "8. Afișarea de statistici referitoare la salariul angajaților. Se va afișa salariul mediu,\n" +
+                    "salariul minim şi salariul maxim. \n" +
+                    "9. Afișarea unor mesaje care indică dacă printre angajați există cel puțin un “Ion”. Se vor\n" +
+                    "afișa mesaje precum: „Firma are cel puțin un Ion angajat”, „Firma nu are nici un Ion\n" +
+                    "angajat”\n" +
+                    "10. Afișarea numărului de persoane care s-au angajat în vara anului precedent.\n");
+            System.out.print("Alegeti optiunea: ");
+            opt = scanner.nextInt();
+            if(opt == 0)
+                break;
+            switch (opt) {
+                case 1:
+                    Angajat.afisare(angajati);
+                    break;
+                case 2:Angajat.afisare_filtrata1(angajati, a -> a.getSalar()>2500);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:Angajat.afisare_filtrata2(angajati, a -> a.getSalar()<3000);
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                default:
+                    System.out.println("Optiune gresita");
+                    break;
+            }
+        }while(true);
+
+    }
 }
